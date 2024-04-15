2bc8e994cc388791c829dc55293d8f915774322d
AndyKuo <andygyr616@gmail.com>
2024-04-15 22:58:56 +0800

    refactor: layout of contact section

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,14 +1,23 @@
+'use client';
+
 import { FC, memo } from "react";
 import Box from "@mui/material/Box";
 import { Link } from "@mui/material";
 import Grid from "@mui/system/Unstable_Grid";
-import { SectionId, SocialLinks } from "@/data/data";
 import Typography from "@mui/material/Typography";
-import Section from "@/components/layout/section";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+
+import Section from "@/components/layout/section";
+import Globe from "@/components/globe";
+import { SectionId, SocialLinks } from "@/data/data";
 
 
 const Contact: FC = memo(function Contact() {
+  const theme = useTheme();
+  const isMobile = !useMediaQuery(theme.breakpoints.up('sm'));
+
   return (
     <Section sectionId={SectionId.Contact}>
       <Box sx={{ m: 4 }}>
@@ -17,7 +26,7 @@ const Contact: FC = memo(function Contact() {
           Get in touch
         </Typography>
 
-        <Typography textAlign="center" sx={{ mb: 8 }}>
+        <Typography sx={{ mb: 8 }} textAlign="center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores atque corporis doloribus ea,
           harum ipsum laudantium, nihil quidem quisquam reprehenderit totam, ut voluptatem! Cupiditate dolore
           molestias quod rem rerum!
@@ -26,19 +35,20 @@ const Contact: FC = memo(function Contact() {
         <Grid container spacing={4}>
 
           {SocialLinks.map((social, idx) => (
-            <Grid xs={12} sm={4} key={idx} textAlign="center">
+            <Grid key={idx} xs={12} sm={4} textAlign={'center'}>
 
               <IconButton aria-label={social.label} href={social.href} target='_blank'>
                 <social.Icon sx={{ width: 64, height: 64 }}/>
               </IconButton>
 
-              <Typography component='div'>
+              <Typography component={'div'}>
                 <Link
                   href={social.href}
                   aria-label={social.label}
                   key={social.label}
                   target='_blank'
                   underline='none'
+                  color='text.primary'
                 >
                   <span>{social.text}</span>
                 </Link>
@@ -48,6 +58,10 @@ const Contact: FC = memo(function Contact() {
           ))}
 
         </Grid>
+
+        <Box sx={{ my: 4, mx: isMobile ? 0 : 8 }} height={isMobile ? 300 : 450}>
+          <Globe/>
+        </Box>
 
       </Box>
     </Section>
