04baee99f2885699ab148afe471b65c1743d8787
AndyKuo <andygyr616@gmail.com>
2024-04-26 22:48:21 +0800

    feat: add short intro in about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -14,37 +14,50 @@ const About: FC = memo(function About() {
   return (
     <Section sectionId={SectionId.About}>
       <Container fixed sx={{ mt: 4 }}>
-        <Grid container>
+        <Grid container sx={{ mx: 1, justifyContent: 'center' }}>
           <Grid xs={12} md={3} alignContent='center'>
-            <Avatar 
-              alt="profile picture" 
-              src={profileImageSrc} 
+            <Avatar
+              alt="profile picture"
+              src={profileImageSrc}
               sx={{ width: 200, height: 200, mx: "auto", my: 2 }}
               component={Paper}
               elevation={8}
             />
           </Grid>
 
-          <Grid container xs={12} md={9}>
-            <Paper elevation={4} sx={{ borderRadius: 4, backgroundColor: 'divider', mx: 4 }}>
-              <Typography component="div" sx={{ m: 2, px: 2 }}>
-                <Box component="h2" sx={{ fontWeight: "bold", textAlign: 'center' }}>About me</Box>
-                <Box component="p" sx={{ color: "text.secondary" }}>{description}</Box>
-              </Typography>
+          <Grid
+            container xs={12} md={6}
+            sx={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}
+          >
 
-              <Grid container sx={{ m: 2 }}>
-                {aboutItems.map(({ label, text, Icon }, idx) => (
-                  <Grid xs={12} sm={6} key={idx} sx={{ p: 2 }}>
-                    <Stack direction="row">
-                      {Icon && <Icon/>}
-                      <Box component="span" sx={{ fontWeight: "bold", mx: 1 }}>{label}:</Box>
-                      <Box component="span" sx={{ color: "text.secondary" }}>{text}</Box>
-                    </Stack>
-                  </Grid>
-                ))}
-              </Grid>
-            </Paper>
+            <Typography component={'h3'} variant={'h3'}>
+              {"I'm Andy Kuo,"}
+            </Typography>
+            <Typography component={'h4'} variant={'h4'}>
+              {"a software engineer."}
+            </Typography>
+
           </Grid>
+
+          <Paper elevation={4} sx={{ borderRadius: 4, backgroundColor: 'divider', mx: 4, mt: 4 }}>
+            <Typography component="div" sx={{ m: 2, px: 2 }}>
+              <Box component="h2" sx={{ fontWeight: "bold", textAlign: 'center' }}>About me</Box>
+              <Box component="p" sx={{ color: "text.secondary" }}>{description}</Box>
+            </Typography>
+
+            <Grid container sx={{ m: 2 }}>
+              {aboutItems.map(({ label, text, Icon }, idx) => (
+                <Grid xs={12} sm={6} key={idx} sx={{ p: 2 }}>
+                  <Stack direction="row">
+                    {Icon && <Icon/>}
+                    <Box component="span" sx={{ fontWeight: "bold", mx: 1 }}>{label}:</Box>
+                    <Box component="span" sx={{ color: "text.secondary" }}>{text}</Box>
+                  </Stack>
+                </Grid>
+              ))}
+            </Grid>
+          </Paper>
+
         </Grid>
       </Container>
     </Section>
