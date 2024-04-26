30e04fe55238509710b9ef7262f6928e0dc1092b
AndyKuo <andygyr616@gmail.com>
2024-04-27 00:40:07 +0800

    refactor: make footer button scroll to top of page

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,14 +1,21 @@
+'use client';
+
 import { FC, memo } from 'react';
 import Box from "@mui/material/Box";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import IconButton from '@mui/material/IconButton';
 import { SectionId } from '@/data/data';
 
+function scrollToTop() {
+  window.scrollTo(0, 0)
+}
+
 const Footer: FC = memo(function Footer() {
     return (
       <Box sx={{ mx: "auto", py: 4, textAlign: "center" }}>
         <Box>
-          <IconButton href={`#${SectionId.About}`} aria-label={SectionId.About}>
+          {/*<IconButton href={`#${SectionId.About}`} aria-label={SectionId.About}>*/}
+          <IconButton aria-label={SectionId.About} onClick={scrollToTop}>
             <KeyboardArrowUpIcon sx={{ width: 40, height: 40 }}/>
           </IconButton>
         </Box>
