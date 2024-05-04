273c52f4e3c921e281c6e6dde0e5a27ccdbd4a46
AndyKuo <andygyr616@gmai.com>
2024-05-05 01:58:50 +0800

    refactor: tune education section layout on mobile

--- src/components/sections/resume/education.tsx
+++ src/components/sections/resume/education.tsx
@@ -1,20 +1,34 @@
+"use client";
+
 import { FC, memo } from "react";
-import { Education as EducationData } from "@/data/data";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import Grid from "@mui/system/Unstable_Grid";
 import Typography from "@mui/material/Typography";
 import { Avatar } from "@mui/material";
 
+import { Education as EducationData } from "@/data/data";
+
 
 const Education: FC = memo(function Education() {
+  const theme = useTheme();
+  const isMobile = !useMediaQuery(theme.breakpoints.up('sm'));
   return (
     <Grid container>
       {EducationData.map(({ date, location, title, logo }, idx) => (
 
-        <Grid key={idx} container xs={12} sm={6}>
+        <Grid key={idx} container xs={12} sm={6} sx={{ my: 4 }}>
           <Grid xs={12} sm={3} display="flex" justifyContent="center">
             <Avatar alt={`${location} logo`} src={logo} sx={{ width: 64, height: 64 }}/>
           </Grid>
-          <Grid xs={12} sm={9}>
+          <Grid 
+            xs={12} 
+            sm={9} 
+            sx={{ 
+              textAlign: isMobile ? 'center' : 'left',
+              mt: isMobile ? 2 : 0,
+            }}
+          >
             <Typography component="div">
               {date}
             </Typography>
