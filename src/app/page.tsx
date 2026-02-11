cd110f073a6bf7acc9c200c8e7082f32a64a01aa
Andy Kuo <andygyr616@gmail.com>
2026-02-11 23:46:50 +0800

    chore: remove unused commented code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,16 @@ import Footer from "@/components/sections/footer";
 import Contact from "@/components/sections/contact";
 
 
-const Header = dynamic(() => import('@/components/sections/header'), {ssr: false});
+const Header = dynamic(() => import('@/components/sections/header'), { ssr: false });
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
-      <Header/>
-      {/*<Paper elevation={4} sx={{ my: 10, borderRadius: 8, backdropFilter: 'blur(4px)' }}>*/}
-      <About/>
-      <Resume/>
-      <Contact/>
-      <Footer/>
-      {/*</Paper>*/}
+      <Header />
+      <About />
+      <Resume />
+      <Contact />
+      <Footer />
     </Container>
   );
 }
